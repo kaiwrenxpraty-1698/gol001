9bf95a33ba3d72fa084cab07e5919a3bf110b06d
pratyush <guptapratyush98@gmail.com>
2022-07-30 11:43:41 +0530

    Starting the naive approach by utilising extra memory space; not taking user input as of now

---
+++ game_of_life_cli.py
@@ -0,0 +1,14 @@
+'''
+Attempt one: 29072022: Trying to come up with a naive approach to the problem
+Rules to this solution:
+1. For this attempt, we will not dwell deep into optimisation. 
+2. I will use a sample list of list to build a matrix for the problem statement.
+'''
+
+matrix = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]];
+
+'''
+Basic background: the output can be returned in a separate memory space or can be achieved by in-place operation. 
+'''
+
+#approach by utilising extra memory
