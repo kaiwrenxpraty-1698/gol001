8c5ee5089b2640f64d8968e89292674b6d8c6553
pratyush <guptapratyush98@gmail.com>
2022-07-30 12:47:51 +0530

    Adding few print statements to get hold of the logic

--- game_of_life_cli.py
+++ game_of_life_cli.py
@@ -20,7 +20,18 @@ def naive_solution(matrix):
 
     for i in range(m):
         for j in range(n):
-              
+
+            # we access the cells of the matrix
+            # we will proceed to count the neighbours of each cell
+
+            living_cells = 0;
+            for x in range(max(i - 1, 0), min(i + 2, m)):
+                for y in range(max(j - 1, 0), min(j + 2, n)):
+                    if x == i and y == j: # if the scanning matrix matches the cell; it simply skips the scan
+                        continue
+                    living_cells += matrix[x][y] % 2;
+                    print(f"this is the row {i} and column {j} and the element {matrix[i][j]}, and {living_cells} and product {matrix[x][y] % 2}");
+
 
 
 print(naive_solution(matrix))
