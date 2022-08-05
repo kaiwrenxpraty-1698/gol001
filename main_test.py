04b9a4a8399d32ea1cef506915f58164db496d47
pratyush <guptapratyush98@gmail.com>
2022-08-06 00:53:33 +0530

    initial commit to the unit testing module

---
+++ main_test.py
@@ -0,0 +1,6 @@
+'''
+Functional test to test the main script
+adding article to be read: https://www.digitalocean.com/community/tutorials/how-to-use-unittest-to-write-a-test-case-for-a-function-in-python
+'''
+import unittest
+from main import initiate_program, start_application
