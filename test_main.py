cb40d339d9cbe2b2b01ba5554e7931367777a19e
pratyush <guptapratyush98@gmail.com>
2022-08-06 23:50:29 +0530

    No success in adding patch to unittest

--- test_main.py
+++ test_main.py
@@ -1,2 +1,8 @@
-def test_main():
-    pass
+from main import *
+from unittest.mock import patch
+
+def test_main(capsys):
+    with patch("sys.argv", ["main", "--option", 2]):
+        main()
+        captured = capsys.readuterr()
+        assert captured.out == 1
