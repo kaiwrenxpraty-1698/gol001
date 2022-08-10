a95bce7ad61e3f4f745e87810b7d9988a49b00b4
pratyush <guptapratyush98@gmail.com>
2022-08-11 00:33:15 +0530

    removing semi colons to improve the look of the code

--- main.py
+++ main.py
@@ -16,14 +16,15 @@ arg_desc = '''\
 '''
 
 summary_dict ={
-    '1' :"This is the most common still life pattern of Game of Life: Block; the state of matrix is identical to the input matrix.",
-    '2':"This is the another common still life pattern of Game of Life: Bee-hive; the state of matrix is identical to the input matrix.",
-    '3':"This is a popular oscillator pattern of Game of Life: Blinker; the matrix reaches its initial state after exactly 2 iterations, hence the pattern keeps oscillating.",
-    '4':"This is a popular oscillator pattern of Game of Life: Toad; the matrix reaches its initial state after exactly 2 iterations, hence the pattern keeps oscillating.",
-    '5':"This is the most popular Spaceships pattern of Game of Life: Glider; the pattern keeps gliding over the matrix",
+    '1' :"This is the most common still life pattern of Game of Life: Block the state of matrix is identical to the input matrix.",
+    '2':"This is the another common still life pattern of Game of Life: Bee-hive the state of matrix is identical to the input matrix.",
+    '3':"This is a popular oscillator pattern of Game of Life: Blinker the matrix reaches its initial state after exactly 2 iterations, hence the pattern keeps oscillating.",
+    '4':"This is a popular oscillator pattern of Game of Life: Toad the matrix reaches its initial state after exactly 2 iterations, hence the pattern keeps oscillating.",
+    '5':"This is the most popular Spaceships pattern of Game of Life: Glider the pattern keeps gliding over the matrix",
     'help': arg_desc,
     'error':'Sorry this option is not valid, please add help argument to know how the tool works'
 }
+
 
 def initiate_program():
     option = sys.argv[1]
@@ -81,21 +82,21 @@ def implement_game_of_life(matrix):
                 for y in range(max(j - 1, 0), min(j + 2, n)):
                     if x == i and y == j:
                         continue
-                    living_cells += matrix[x][y] % 2;        
+                    living_cells += matrix[x][y] % 2        
             output_matrix[i].append(living_cells)
 
     for i in range(m):
         for j in range(n):
             if matrix[i][j] == 1:
                 if output_matrix[i][j] == 2 or output_matrix[i][j] == 3:
-                    output_matrix[i][j] = 1;   
+                    output_matrix[i][j] = 1   
                 else:
-                    output_matrix[i][j] = 0;
+                    output_matrix[i][j] = 0
             else:
                 if output_matrix[i][j] == 3:
-                    output_matrix[i][j] = 1;
+                    output_matrix[i][j] = 1
                 else:
-                    output_matrix[i][j] = 0;
+                    output_matrix[i][j] = 0
                     
     return output_matrix
 
@@ -110,45 +111,45 @@ def start_application(option):
         matrix = select_matrix_option(option)
         output_matrix = implement_game_of_life(matrix)
         masked_matrix = mask_input_matrix(output_matrix)
-        print(f"Here is your output matrix -->");
-        print();
-        print_matrix(masked_matrix);
-        print();
+        print(f"Here is your output matrix -->")
+        print()
+        print_matrix(masked_matrix)
+        print()
         summary(option)    
     elif option == "3" or option == "4":
         matrix = select_matrix_option(option)
-        count = 0;
+        count = 0
         while count < 10:
-            matrix = implement_game_of_life(matrix);
-            masked_matrix = mask_input_matrix(matrix);
+            matrix = implement_game_of_life(matrix)
+            masked_matrix = mask_input_matrix(matrix)
             sleep(.5)
-            print(f"Here is your output matrix -->");
-            print();
-            print_matrix(masked_matrix);
-            print();
+            print(f"Here is your output matrix -->")
+            print()
+            print_matrix(masked_matrix)
+            print()
             print(f"State of the matrix --> {count%2}")
-            count += 1;
-            print();
-        summary(option);
+            count += 1
+            print()
+        summary(option)
     elif option == "5":
         matrix = select_matrix_option(option)
-        count = 0;
+        count = 0
         while count < 25:
-            matrix = implement_game_of_life(matrix);
-            masked_matrix = mask_input_matrix(matrix);
+            matrix = implement_game_of_life(matrix)
+            masked_matrix = mask_input_matrix(matrix)
             sleep(.75)
-            print(f"Here is your output matrix -->");
-            print();
-            print_matrix(masked_matrix);
-            print();
-            count += 1;
-        summary(option);
+            print(f"Here is your output matrix -->")
+            print()
+            print_matrix(masked_matrix)
+            print()
+            count += 1
+        summary(option)
     else:
         return 0
 
 def main():
-    option = initiate_program();
-    start_application(option);
+    option = initiate_program()
+    start_application(option)
 
 
 if __name__ == "__main__":
