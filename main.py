6db9335c72b26958bbef6415e1982ed8da44f7e5
pratyush <guptapratyush98@gmail.com>
2022-08-11 01:16:12 +0530

    minor changes to remove unwanted variables

--- main.py
+++ main.py
@@ -44,16 +44,30 @@ def introduction(option):
     name_of_pattern = summary_dict['mapping'][option]
     print(f'Welcome to the Game of Life; So glad that you have chosen {name_of_pattern}')
 
+def print_matrix(matrix):
+    for idx in range(len(matrix)):
+            print(matrix[idx])
+
+def mask_input_matrix(matrix): 
+    output_matrix = [[] for _ in range(len(matrix))]
+    for idx in range(len(matrix)):
+        for idx2 in range(len(matrix[0])):
+            if matrix[idx][idx2] == 0:
+                output_matrix[idx].append(" ")
+            else:
+                output_matrix[idx].append("ALIVE")
+    return output_matrix
+
 def initiate_program():
     option = sys.argv[1]
     if option != "help":
-        print_matrix(mask_input_matrix(select_matrix_option(option)))
+        print_matrix(mask_input_matrix(select_from_matrix_option(option)))
         return option
     else:
         print(summary_dict[option])
         return option
 
-def select_matrix_option(option):
+def select_from_matrix_option(option):
     live_cell, dead_cell = 1, 0
     if option == "1":
         matrix = [[dead_cell, dead_cell, dead_cell, dead_cell], [dead_cell, live_cell, live_cell, dead_cell], [dead_cell, live_cell, live_cell, dead_cell], [dead_cell, dead_cell, dead_cell, dead_cell]]
@@ -71,20 +85,6 @@ def select_matrix_option(option):
         print(summary_dict['error'])
         exit()
     return matrix
-
-def print_matrix(matrix):
-    for idx in range(len(matrix)):
-            print(matrix[idx])
-
-def mask_input_matrix(matrix): 
-    output_matrix = [[] for _ in range(len(matrix))]
-    for idx in range(len(matrix)):
-        for idx2 in range(len(matrix[0])):
-            if matrix[idx][idx2] == 0:
-                output_matrix[idx].append(" ")
-            else:
-                output_matrix[idx].append("ALIVE")
-    return output_matrix
                 
 def implement_game_of_life(matrix):
     output_matrix = [[] for _ in range(len(matrix))]
@@ -115,39 +115,32 @@ def implement_game_of_life(matrix):
     return output_matrix
 
 def start_application(option):
-
-
     if option == "1" or option == "2":
         introduction(option)
-        matrix = select_matrix_option(option)
-        output_matrix = implement_game_of_life(matrix)
-        masked_matrix = mask_input_matrix(output_matrix)
+        matrix = select_from_matrix_option(option)
+        matrix = implement_game_of_life(matrix)
         print(f"Here is your output matrix -->")
-        print_matrix(masked_matrix)
+        print_matrix(mask_input_matrix(matrix))
         summary(option)    
     elif option == "3" or option == "4":
-        matrix = select_matrix_option(option)
+        matrix = select_from_matrix_option(option)
         count = 0
         while count < 10:
             matrix = implement_game_of_life(matrix)
-            masked_matrix = mask_input_matrix(matrix)
             sleep(.5)
             print(f"Here is your output matrix -->")
-            print_matrix(masked_matrix)
+            print_matrix(mask_input_matrix(matrix))
             print(f"State of the matrix --> {count%2}")
             count += 1
         summary(option)
     elif option == "5":
-        matrix = select_matrix_option(option)
+        matrix = select_from_matrix_option(option)
         count = 0
         while count < 25:
             matrix = implement_game_of_life(matrix)
-            masked_matrix = mask_input_matrix(matrix)
             sleep(.75)
             print(f"Here is your output matrix -->")
-            
-            print_matrix(masked_matrix)
-            
+            print_matrix(mask_input_matrix(matrix))
             count += 1
         summary(option)
     else:
