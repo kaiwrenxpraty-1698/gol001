534d67365f2566d91a5faf3988f3e9f923b67807
pratyush <guptapratyush98@gmail.com>
2022-08-15 01:12:10 +0530

    attempt to implement unit testing based on unit test written for an API at work

--- test_main.py
+++ test_main.py
@@ -1,8 +1,21 @@
-from main import *
-from unittest.mock import patch
+import sys
+import main
 
-def test_main(capsys):
-    with patch("sys.argv", ["main", "--option", 2]):
-        main()
-        captured = capsys.readuterr()
-        assert captured.out == 1
+def test_select_from_matrix_option():
+    option = "5"
+    matrix = main.select_from_matrix_option(option)
+    ground_truth_matrix = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0, 0]] + [[0 for _ in range(10)] for _ in range(7)]
+
+    assert matrix == ground_truth_matrix
+
+def test_implement_game_of_life():
+    matrix = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0, 0]] + [[0 for _ in range(10)] for _ in range(7)]
+    output_matrix = main.implement_game_of_life(matrix)
+    ground_truth_matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]] + [[0 for _ in range(10)] for _ in range(6)]
+
+    assert output_matrix == ground_truth_matrix
+
+if __name__ == "__main__":
+    test_select_from_matrix_option()
+    test_implement_game_of_life()
+
