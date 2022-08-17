21ea4af774d4b577684adc4f42d7d35ebaf85f19
Sidu Ponnappa <ckponnappa@gmail.com>
2022-08-18 02:39:33 +0800

    - Refactor logic to compute alive neighbour counts using extract method - Decouple the modeling of alive neighbour counts from the modeling of the output matrix

--- gameoflife/next_generation.py
+++ gameoflife/next_generation.py
@@ -2,32 +2,39 @@ from gameoflife.constants import ALIVE, DEAD
 
 
 def next_generation_of(matrix):
-    output_matrix = [[] for _ in range(len(matrix))]
+    matrix_of_alive_neighbour_counts = _compute_number_of_living_neighbours_for(matrix)
+    output_matrix = [row[:] for row in matrix]
     rows, columns = len(matrix), len(matrix[0])
-    for row in range(rows):
-        for column in range(columns):
-            living_cells = 0
-            for x in _neighbours_positions_for(row, rows):
-                for y in _neighbours_positions_for(column, columns):
-                    if _is_looking_outside_the_matrix_boundary(column, row, x, y):
-                        continue
-                    living_cells += matrix[x][y] % 2
-            output_matrix[row].append(living_cells)
 
     for row in range(rows):
         for column in range(columns):
             if matrix[row][column] == ALIVE:
-                if output_matrix[row][column] == 2 or output_matrix[row][column] == 3:
+                if matrix_of_alive_neighbour_counts[row][column] == 2 or \
+                        matrix_of_alive_neighbour_counts[row][column] == 3:
                     output_matrix[row][column] = ALIVE
                 else:
                     output_matrix[row][column] = DEAD
             else:
-                if output_matrix[row][column] == 3:
+                if matrix_of_alive_neighbour_counts[row][column] == 3:
                     output_matrix[row][column] = ALIVE
                 else:
                     output_matrix[row][column] = DEAD
-
     return output_matrix
+
+
+def _compute_number_of_living_neighbours_for(matrix):
+    living_cell_count_matrix = [[] for _ in range(len(matrix))]
+    rows, columns = len(matrix), len(matrix[0])
+    for row in range(rows):
+        for column in range(columns):
+            living_cell_count = 0
+            for x in _neighbours_positions_for(row, rows):
+                for y in _neighbours_positions_for(column, columns):
+                    if _is_looking_outside_the_matrix_boundary(column, row, x, y):
+                        continue
+                    living_cell_count += matrix[x][y] % 2
+            living_cell_count_matrix[row].append(living_cell_count)
+    return living_cell_count_matrix
 
 
 def _is_looking_outside_the_matrix_boundary(column, row, x, y):
