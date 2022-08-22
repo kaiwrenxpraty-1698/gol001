004b0826dc3abef6259dbfb1e5c912d759e979ff
pratyush <guptapratyush98@gmail.com>
2022-08-22 12:47:21 +0530

    - Add class references to initialise input output submodule related dependencies

--- tests/test_input_output.py
+++ tests/test_input_output.py
@@ -1,7 +1,7 @@
 import unittest
 
 from gameoflife.constants import DEAD, ALIVE
-from gameoflife.input_output import select_menu_option
+from gameoflife.input_output import InputOutput
 
 
 class TestInputOutput(unittest.TestCase):
@@ -12,7 +12,7 @@ class TestInputOutput(unittest.TestCase):
                                   [ALIVE, ALIVE, ALIVE, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD]
                                   ] + [[DEAD for _ in range(10)] for _ in range(7)]
 
-        self.assertEqual(select_menu_option(option), expected_output_matrix)
+        self.assertEqual(InputOutput(option, None).select_menu_option(), expected_output_matrix)
 
 
 if __name__ == "__main__":
