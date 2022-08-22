0879b78360ebf3cfe1355645a10899b96ff6e7f9
pratyush <guptapratyush98@gmail.com>
2022-08-22 13:51:42 +0530

    - Remove summary static dictionary from constants.py

--- gameoflife/constants.py
+++ gameoflife/constants.py
@@ -7,20 +7,3 @@ BLOCK_PATTERN = [
     [DEAD, ALIVE, ALIVE, DEAD],
     [DEAD, DEAD, DEAD, DEAD]
 ]
-
-SUMMARY_DICT = {
-    "1" :"This is the most common still life pattern of Game of Life: Block. \nKey point: the state of matrix is identical to the input matrix.",
-    "2":"This is the another common still life pattern of Game of Life: Bee-hive. \nKey point: the state of matrix is identical to the input matrix.",
-    "3":"This is a popular oscillator pattern of Game of Life: Blinker. \nKey point: the matrix reaches its initial state after exactly 2 iterations, hence the pattern keeps oscillating.",
-    "4":"This is a popular oscillator pattern of Game of Life: Toad. \nKey point: the matrix reaches its initial state after exactly 2 iterations, hence the pattern keeps oscillating.",
-    "5":"This is the most popular Spaceships pattern of Game of Life: Glider. \nKey point: the pattern keeps gliding over the matrix",
-    "help": "Welcome to the Game of Life.\nPlease select the option number corresponding to the pattern, you will like to print: \n1. Block \n2. Bee-Hive \n3. Blinker \n4. Toad \n5. Glider",
-    "error":"Sorry this option is not valid, please add help argument to know how the tool works",
-    "mapping":{
-        "1": "Block",
-        "2": "Bee-Hive",
-        "3": "Blinker",
-        "4": "Toad",
-        "5": "Glider"
-}
-}
