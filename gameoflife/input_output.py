059b9882bb2e4c4d828865a9c4622348f4df61c9
pratyush <guptapratyush98@gmail.com>
2022-08-22 12:44:29 +0530

    - Add Docstrings for linting

--- gameoflife/input_output.py
+++ gameoflife/input_output.py
@@ -12,30 +12,43 @@ class InputOutput:
     def __init__(self, option=None, matrix=None):
         self.option = option
         self.matrix = matrix
+
     def start_introduction(self):
         '''
         method to load introduction summary from the summary dictionary
         '''
         name_of_pattern = SUMMARY_DICT['mapping'][self.option]
         print(f'Welcome to the Game of Life; So glad that you have chosen: {name_of_pattern}')
+
     def end_summary(self):
         '''
         method to load ending summary from the summary dictionary
         '''
         print(SUMMARY_DICT[self.option])
+
     def print_matrix(self):
         '''
         method to print matrix
         '''
         for idx in range(len(self.matrix)):
             print(self.matrix[idx])
+
     def output_print_statement(self, state):
+        '''
+        method to print dynamic strings
+        '''
         print(f"Here is your {state} matrix -->")
     
     def output_generation_statement(self, number_of_generation):
+        '''
+        method to print generation of iteration dynamically
+        '''
         print(f"Generation of the matrix --> {number_of_generation}")
 
     def mask_input_matrix_values(self): 
+        '''
+        method to mask underlying nummeric values with constants
+        '''
         output_matrix = [[] for _ in range(len(self.matrix))]
         for idx in range(len(self.matrix)):
             for idx2 in range(len(self.matrix[0])):
